e03827aa54c1fdfab280cce89bb5a05c7c6c1b56
Charles <enquiries@movingdigi.com>
2017-01-18 16:57:36 +0000

    enabling discovery by eureka service

--- src/main/java/microservice/MicroService.java
+++ src/main/java/microservice/MicroService.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @RestController
 @SpringBootApplication
 public class MicroService {
+
 
   @RequestMapping(value = "/available")
   public String available() {
