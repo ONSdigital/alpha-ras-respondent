533adfbd466403e72de93d5ca199fd39b3136925
Charles <enquiries@movingdigi.com>
2017-01-18 15:09:49 +0000

    microservice stub app, for 1-to-1 mapping from gateway to service

---
+++ src/main/java/microservice/MicroService.java
@@ -0,0 +1,27 @@
+package microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class MicroService {
+
+  @RequestMapping(value = "/available")
+  public String available() {
+    return "Spring in Action";
+  }
+
+  @RequestMapping(value = "/checked-out")
+  public String checkedOut() {
+    return "Spring Boot in Action";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(MicroService.class, args);
+  }
+}
+
+
